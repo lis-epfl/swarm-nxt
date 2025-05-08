3c05ff59fef2b9f3b6d644580c876482d1333978
Niel Mistry <niel@nielmistry.com>
2025-05-08 14:20:48 +0200

    add rms offset check in chronyc_check

--- ansible/scripts/chronyc_check.py
+++ ansible/scripts/chronyc_check.py
@@ -6,10 +6,24 @@ import re
 sp = subprocess.run(["chronyc", "tracking"], capture_output=True)
 output = sp.stdout.decode('utf-8')
 
-ref_id = re.search("Reference ID\\s+:\\s+([A-Z0-9]{8})", output).group(1)
+ref_id = re.search("Reference ID\\s+:\\s+([A-F0-9]{8})", output).group(1)
+if ref_id: 
+    ref_id = int(ref_id, 16)
 
-print(f"Found Reference ID: {ref_id}")
-if not ref_id or ref_id == "00000000":
+print(f"Found Reference ID: 0x{ref_id:X}")
+if not ref_id:
+    exit(1)
+
+# check rms offset
+rms_offset = re.search("RMS offset\\s+:\\s+([0-9]?\\.[0-9]+) seconds", output).group(1)
+if not rms_offset: 
+    print("Did not get an RMS offset...")
+    exit(1)
+
+rms_offset = float(rms_offset)
+print(f"RMS Offset: {rms_offset}")
+if rms_offset > 1.0: 
+    print("Too slow!")
     exit(1)
 
 exit(0)
