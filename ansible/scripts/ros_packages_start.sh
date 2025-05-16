d5ba90c777fa89251b09067b0bede2e1eed20c5f
Niel Mistry <niel.mistry@epfl.ch>
2025-05-16 17:18:15 +0200

    reduce reliance on sudo, better launch file

--- ansible/scripts/ros_packages_start.sh
+++ ansible/scripts/ros_packages_start.sh
@@ -2,8 +2,8 @@
 set -exo pipefail 
 export ROS_DOMAIN_ID={{ ros_domain_id }}
 
-cd {{ drone_ros_path }}
+cd {{ ros_path }}
 source /opt/ros/humble/setup.bash
 source install/setup.bash
 
-ros2 launch {{ drone_base_path }}/launch.yaml
+ros2 launch {{ base_path }}/launch.py
