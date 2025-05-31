91dc7921ae269549663be7d51f717e1188eb338f
Niel Mistry <niel@nielmistry.com>
2025-05-31 22:09:17 +0200

    ansible: use check and launch to launch mocap to vision pose

--- ansible/scripts/ros_packages_start.sh
+++ ansible/scripts/ros_packages_start.sh
@@ -6,4 +6,6 @@ cd {{ ros_path }}
 source /opt/ros/humble/setup.bash
 source install/setup.bash
 
-ros2 launch {{ base_path }}/launch.py
+
+ros2 launch {{ base_path }}/launch.py &
+ros2 run mocap_to_vision_ros2 mocap_to_vision_pose_ros2 check_and_launch.py --namespace {{ ns }} --mocap-topic  "/optitrack_multiplexer_node/rigid_body/{{ ansible_hostname }}"
