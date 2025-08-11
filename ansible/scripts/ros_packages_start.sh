8d072751ea5b3fdb48e258d784c8cb873b88abd6
Niel Mistry <niel@nielmistry.com>
2025-08-11 18:56:21 +0200

    make folder

--- ansible/scripts/ros_packages_start.sh
+++ ansible/scripts/ros_packages_start.sh
@@ -3,6 +3,7 @@ set -exo pipefail
 export ROS_DOMAIN_ID={{ ros_domain_id }}
 export ROS_LOG_DIR={{ base_path }}/logs/$(date +%Y/%m/%d/%H%M%S)
 
+mkdir -p {{ base_path }}/logs
 ln -sfn $ROS_LOG_DIR {{ base_path }}/logs/latest
 
 cd {{ ros_path }}
