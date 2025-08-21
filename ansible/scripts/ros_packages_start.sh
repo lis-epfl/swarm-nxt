469b4b7c5f12d4e6bc8283c68e037e01b7883f77
Niel Mistry <niel.mistry@epfl.ch>
2025-08-21 16:20:16 +0200

    drone num

--- ansible/scripts/ros_packages_start.sh
+++ ansible/scripts/ros_packages_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -exo pipefail 
-export ROS_DOMAIN_ID={{ ros_domain_id }}
+export ROS_DOMAIN_ID={{ drone_num }}
 export ROS_LOG_DIR={{ base_path }}/logs/$(date +%Y/%m/%d/%H%M%S)
 
 mkdir -p {{ base_path }}/logs
@@ -11,7 +11,11 @@ source /opt/ros/humble/setup.bash
 source install/setup.bash
 
 # Stagger start times: two seconds per number
+<<<<<<< HEAD
 sleep {{ 0 if (drone_num | int) == 0 else (2*( drone_num | int))  }}
+=======
+sleep {{ 2*(( drone_num | int) - 1) }}
+>>>>>>> dabb432 (drone num)
 # Use exec to replace the shell process with ros2 launch
 # This ensures signals (SIGTERM) from systemd are properly forwarded
 exec ros2 launch {{ base_path }}/launch.py 
