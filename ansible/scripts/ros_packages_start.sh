02f0261a9baf1dde0e364a912c582e4f37a57091
Niel Mistry <niel.mistry@epfl.ch>
2025-08-21 14:02:37 +0200

    add stagger

--- ansible/scripts/ros_packages_start.sh
+++ ansible/scripts/ros_packages_start.sh
@@ -10,6 +10,8 @@ cd {{ ros_path }}
 source /opt/ros/humble/setup.bash
 source install/setup.bash
 
+# Stagger start times: two seconds per number
+sleep {{ 0 if (drone_num | int) == 0 else (2*( drone_num | int))  }}
 # Use exec to replace the shell process with ros2 launch
 # This ensures signals (SIGTERM) from systemd are properly forwarded
 exec ros2 launch {{ base_path }}/launch.py 
