42782388a08474ca2a2cf3d5831932f34c7d8d6d
Niel Mistry <niel.mistry@epfl.ch>
2025-08-22 14:35:27 +0200

    remove merge conflict remnants

--- ansible/scripts/ros_packages_start.sh
+++ ansible/scripts/ros_packages_start.sh
@@ -11,11 +11,7 @@ source /opt/ros/humble/setup.bash
 source install/setup.bash
 
 # Stagger start times: two seconds per number
-<<<<<<< HEAD
 sleep {{ 0 if (drone_num | int) == 0 else (2*( drone_num | int))  }}
-=======
-sleep {{ 2*(( drone_num | int) - 1) }}
->>>>>>> dabb432 (drone num)
 # Use exec to replace the shell process with ros2 launch
 # This ensures signals (SIGTERM) from systemd are properly forwarded
 exec ros2 launch {{ base_path }}/launch.py 
