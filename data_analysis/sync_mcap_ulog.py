9f75665a696ba11b75253a3ff2e5014e4533eac9
Niel Mistry <niel.mistry@epfl.ch>
2025-09-05 18:45:43 +0200

    start scripts

---
+++ data_analysis/sync_mcap_ulog.py
@@ -0,0 +1,10 @@
+import rosbag2_py
+from rclpy.serialization import deserialize_message
+from pyulog import ULog
+import typer
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    typer.run(main)
