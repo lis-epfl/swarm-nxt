de4dd113899643f9c460d655ee67dd1830be83fd
Charbel Toumieh <charbel.toumieh@epfl.ch>
2025-11-15 23:24:58 +0100

    updage

--- ansible/templates/local_build.sh
+++ ansible/templates/local_build.sh
@@ -4,8 +4,5 @@ set -e
 # Source ROS environment
 source /opt/ros/humble/setup.sh
 
-# Build multi-agent planner messages
-colcon build --base-paths multi_agent_pkgs --packages-select multi_agent_planner_msgs
-
 # Build main packages
-colcon build --base-paths src
+colcon build --symlink-install src
